64051becbdb204f7d7ce25038449753d423c787d
TomazMartins <tomaz.r.martins@gmail.com>
2017-03-21 16:02:49 -0300

    Initial commit
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

---
+++ app/src/main/java/br/com/algorithmworks/samplerequery/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.algorithmworks.samplerequery.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.algorithmworks.samplerequery.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
