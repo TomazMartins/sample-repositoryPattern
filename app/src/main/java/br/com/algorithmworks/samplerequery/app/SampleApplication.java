be59105f961eb9448deda4e50c80ac7cbf9a4aa1
TomazMartins <tomaz.r.martins@gmail.com>
2017-03-21 16:29:34 -0300

    Creating only one connection to database in Application
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

---
+++ app/src/main/java/br/com/algorithmworks/samplerequery/app/SampleApplication.java
@@ -0,0 +1,50 @@
+package br.com.algorithmworks.samplerequery.app;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+import io.requery.Persistable;
+import io.requery.android.sqlite.DatabaseSource;
+import io.requery.meta.EntityModel;
+import io.requery.sql.Configuration;
+import io.requery.sql.EntityDataStore;
+
+
+public class SampleApplication extends Application {
+    private EntityDataStore<Persistable> dataStore;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        StrictMode.enableDefaults();
+    }
+
+    /**
+     * Return a single instance of the EntityDataStore
+     *   of the application.
+     *
+     * @return the EntityDataStore responsible to connection with database.
+     */
+    public EntityDataStore<Persistable> getDataStore() {
+        EntityModel models = null;
+
+
+        // Guarantee that there is only one connection with the database.
+        if( dataStore == null ) {
+            /*
+            * Create the connection with the database.
+            *
+            *   As parameters: a 'context', a 'EntityModel' and a number,
+            *   indicate the version of the database.
+            * */
+            DatabaseSource source = new DatabaseSource( this, models, 1 );
+
+            Configuration configuration = source.getConfiguration();
+
+            dataStore = new EntityDataStore<Persistable>( configuration );
+        }
+
+        return dataStore;
+    }
+}
