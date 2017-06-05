bb0d10ba64fc46c4c4a46bf6746cd14f4f60e830
TomazMartins <tomaz.r.martins@gmail.com>
2017-06-05 11:47:34 -0300

    Adding the setters
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- app/src/main/java/br/com/tomazmartins/samplerequery/external/infrastructure/db/entity/CountryEntity.java
+++ app/src/main/java/br/com/tomazmartins/samplerequery/external/infrastructure/db/entity/CountryEntity.java
@@ -3,6 +3,7 @@ package br.com.tomazmartins.samplerequery.external.infrastructure.db.entity;
 
 import io.requery.CascadeAction;
 import io.requery.Entity;
+import io.requery.ForeignKey;
 import io.requery.Generated;
 import io.requery.Key;
 import io.requery.OneToOne;
@@ -12,13 +13,46 @@ import io.requery.Table;
 @Entity( name = "CountryEntityImpl" )
 @Table( name = "Country" )
 public interface CountryEntity extends Persistable {
+    /*
+    * KEY:
+    *   The attribute ID is the Primary Key.
+    *
+    * GENERATED:
+    *   The attribute ID is auto incremented.
+    * */
     @Key
     @Generated
     int getId();
 
+    /*
+    * ONE-TO-ONE:
+    *   The relationship between PRESIDENT and COUNTRY
+    *   is one-to-one, because a COUNTRY must has only
+    *   one PRESIDENT
+    *   and
+    *   a PRESIDENT is the PRESIDENT
+    *   of only one COUNTRY.
+    *
+    * FOREIGN KEY:
+    *   The Foreign Key was put in here, because
+    *   the entity (Table) COUNTRY 'need' a
+    *   PRESIDENT.
+    *
+    *   The dependence is between COUNTRY -> PRESIDENT,
+    *   not PRESIDENT -> COUNTRY.
+    *
+    *   So, The Foreign Key is put in the dependent
+    *   table. In this case, the 'COUNTRY'.
+    * */
     @OneToOne( cascade = CascadeAction.NONE )
+    @ForeignKey
     PresidentEntity getPresident();
 
     int getPopulation();
     String getName();
+
+    void setPopulation( int population );
+    void setName( String name );
+    void setId( int id );
+    void setPresident( PresidentEntity president );
 }
--- app/src/main/java/br/com/tomazmartins/samplerequery/external/infrastructure/db/entity/PresidentEntity.java
+++ app/src/main/java/br/com/tomazmartins/samplerequery/external/infrastructure/db/entity/PresidentEntity.java
@@ -3,6 +3,7 @@ package br.com.tomazmartins.samplerequery.external.infrastructure.db.entity;
 
 import io.requery.CascadeAction;
 import io.requery.Entity;
+import io.requery.ForeignKey;
 import io.requery.Generated;
 import io.requery.Key;
 import io.requery.OneToOne;
@@ -12,12 +13,32 @@ import io.requery.Table;
 @Entity( name = "PresidentEntityImpl" )
 @Table( name = "PresidentEntity" )
 public interface PresidentEntity extends Persistable {
+    /*
+    * KEY:
+    *   The attribute ID is the Primary Key.
+    *
+    * GENERATED:
+    *   The attribute ID is auto incremented.
+    * */
     @Key
     @Generated
     int getId();
 
+    /*
+    * ONE-TO-ONE:
+    *   The relationship between PRESIDENT and COUNTRY
+    *   is one-to-one, because a COUNTRY must has only
+    *   one PRESIDENT
+    *   and
+    *   a PRESIDENT is the PRESIDENT
+    *   of only one COUNTRY.
+    * */
     @OneToOne( cascade = CascadeAction.NONE )
     CountryEntity getCountry();
 
     String getName();
+
+    void setCountry( CountryEntity country );
+    void setName( String name );
+    void setId( int id );
 }
